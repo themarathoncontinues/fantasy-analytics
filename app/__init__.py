081c05a4db00ded9a9c348508a64aaf331ede6e5
Mitchell Bregman <mitchbregs@gmail.com>
2019-11-03 13:30:00 -0500

    flask app basics

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+"""Initialize app."""
+from flask import Flask
+
+
+def create_app():
+    """Construct the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+
+    # Application Configuration
+    app.config.from_object('config.Config')
+
+    # Initialize Plugins
+
+    return app
