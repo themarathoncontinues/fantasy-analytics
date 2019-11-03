c6c3854a378cdf280253464f32c2f60bfdcbf04c
leonkozlowski <leonkozlowski@gmail.com>
2019-11-03 16:08:45 -0500

    cleanup

--- notebooks/leauge_tester.py
+++ notebooks/leauge_tester.py
@@ -20,32 +20,3 @@ cookies = {
 }
 
 roster = League(league_id=69561285, year=2020, team_id=4, cookies=cookies)._calculate_totals()
-
-# entries = roster['roster'].get('entries')
-#
-# for player in entries:
-#     team_stats = []
-#     stats = get_nested(player, 'playerPoolEntry.player.stats')
-#
-#     for statline in stats:
-#         if statline.get('statSplitTypeId') == 2:
-#             season_totals = statline.get('stats')
-#
-#             relevant_items = dict((STATS_INT_TO_STRING[k], v) for (k, v) in season_totals.items()
-#                                   if k in STATS_INT_TO_STRING.keys())
-#
-#             player_metadata = {
-#                 'name': get_nested(player, 'playerPoolEntry.player.fullName'),
-#                 'id': get_nested(player, 'playerPoolEntry.player.id'),
-#                 'stats': {
-#                     'points': relevant_items.get('points'),
-#                     'blocks': relevant_items.get('blocks'),
-#                     'steals': relevant_items.get('steals'),
-#                     'assists': relevant_items.get('assists'),
-#                     'rebounds': relevant_items.get('rebounds')
-#                 }
-#             }
-#
-#             team_stats.append(player_metadata)
-#
-#     print(team_stats)
