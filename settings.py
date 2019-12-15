b9c5e153434cf839acb1b5a64e5e0baaaa0e7c0f
Mitchell Bregman <mitchbregs@gmail.com>
2019-12-15 01:33:36 -0500

    initial postgres commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+POSTGRES_DBNAME = os.environ.get('POSTGRES_DBNAME')
+POSTGRES_USERNAME = os.environ.get('POSTGRES_USERNAME')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
---
+++ src/tasks/models.py
@@ -0,0 +1,25 @@
+import prefect
+from prefect.tasks.postgres import PostgresFetch
+
+from settings import (
+    POSTGRES_DBNAME,
+    POSTGRES_USERNAME,
+    POSTGRES_PASSWORD,
+    POSTGRES_HOST,
+    POSTGRES_PORT,
+)
+
+
+existing = PostgresFetch(
+    name='query-existing-tables',
+    db_name=POSTGRES_DBNAME,
+    user=POSTGRES_USERNAME,
+    password=POSTGRES_PASSWORD,
+    host=POSTGRES_HOST,
+    port=POSTGRES_PORT,
+    fetch="all",
+    tags=["projects"],
+    query="""
+    SELECT * FROM information_schema.tables
+    """
+)
