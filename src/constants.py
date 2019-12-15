26d2fd9cbf34441a9f798333c5c31ee00d052a82
leonkozlowski <leonkozlowski@gmail.com>
2019-12-15 00:08:08 -0500

    some cleanup

--- src/constants.py
+++ src/constants.py
@@ -1,17 +1,22 @@
+# pylint: skip-file
+# flake8: noqa
+
 ########
 # ESPN #
 ########
 
-FBA_ENDPOINT = 'https://fantasy.espn.com/apis/v3/games/fba/seasons/'
+FBA_ENDPOINT = "https://fantasy.espn.com/apis/v3/games/fba/seasons/"
+URL_API_KEY = "https://registerdisney.go.com/jgc/v5/client/ESPN-FANTASYLM-PROD/api-key?langPref=en-US"
+URL_LOGIN = "https://ha.registerdisney.go.com/jgc/v5/client/ESPN-FANTASYLM-PROD/guest/login?langPref=en-US"
 
 #################
 # STATS MAPPING #
 #################
 
 STATS_INT_TO_STRING = {
-    '0': 'points',
-    '1': 'blocks',
-    '2': 'steals',
-    '3': 'assists',
-    '6': 'rebounds'
+    "0": "points",
+    "1": "blocks",
+    "2": "steals",
+    "3": "assists",
+    "6": "rebounds",
 }
