74de36d3325764ea111a4ab591c6c4d50ee9d536
leonkozlowski <leonkozlowski@gmail.com>
2019-12-22 15:45:01 -0500

    rm installs

--- pr-prep.sh
+++ pr-prep.sh
@@ -7,25 +7,23 @@ export PYTHONPATH=$PWD
 echo $PYTHONPATH
 
 echo "Executing black code formatting"
-pip install black
 echo " >> see (https://github.com/psf/black)"
 black src/
 echo -ne '\n'
 
 
 echo "Executing pylint - python linter"
-pip install pylint
 echo " >> see (https://github.com/PyCQA/pylint)"
 pylint src/
 echo -ne '\n'
 
 
 echo "Executing flake8 - python quality/style linter"
-pip install flake8
 echo " >> see (https://github.com/PyCQA/flake8)"
 flake8 src/
 echo -ne '\n'
 
+echo "Executing pytest - running pytest tests/"
 
 pytest --cov-report term-missing --cov=src/ -vv tests/
 
