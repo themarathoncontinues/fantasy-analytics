4d68971bea0b103d71136f7db5f63f795c826f3b
leonkozlowski <leonkozlowski@gmail.com>
2019-12-29 01:24:28 -0500

    nl

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False)
 
     # Application Configuration
-    app.config.from_object('config.Config')
+    app.config.from_object("config.Config")
 
     # Initialize Plugins
 
